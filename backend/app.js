934961cf50e3869674a7e93145b7548acd589235
professordiogodev <diogo@dionamite.com>
2026-03-17 21:29:31 +0000

    Starting to uses hashes for the copy paste

--- backend/app.js
+++ backend/app.js
@@ -14,38 +14,41 @@ app.use(helmet());
 // Aceitar texto para a rota /copy
 app.use(express.text());
 
-// Valor guardado em memória
-let content = ""
+let content = {
+    "a3175b39145aabc": {
+        createdAt: new Date("2026-03-17 21:14"),
+        value: "Este é o valor do diogo"
+    }
+}
 
 app.get('/', (req, res) => {
-    res.send('Página em React.')
+    res.send('Página Home.')
 })
 
-// Enviar dados de registo
-app.post('/register', (req, res) => {
-    res.send('Registar.')
-})
 
-// Enviar dados de login
-app.post('/login', (req, res) => {
-    // Se o login estiver correto, enviar um jwt
-    res.send('Fazer login.')
+app.get('/get-all-contents', (req, res) => {
+    res.json(content)
 })
 
 app.post('/copy', (req, res) => {
     // Se o jwt existir e houver body, substituir o content
-    if (!req.body){
+    if (!req.body) {
         res.status(400).send("No content in body.")
     }
     else {
-        content = req.body
-        res.send('OK')
+        const newId = crypto.randomUUID()
+        content[newId] = {
+            createdAt: new Date(),
+            value: req.body
+        }
+        res.send(`/${newId}`)
     }
 })
 
-app.get('/paste', (req, res) => {
+app.get('/:id', (req, res) => {
+    const id = req.params.id
     // Se o jwt existir, enviar o content
-    res.send(content)
+    res.send(JSON.stringify(content[id].value))
 })
 
 app.listen(PORT, () => {
