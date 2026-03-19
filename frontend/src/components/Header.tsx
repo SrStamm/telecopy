2f7e50a75e838d77df97c345d079174cf2a643f6
SrStamm <mirkostamm19@gmail.com>
2026-03-19 17:57:21 +0000

    feat: change view between 'send' and 'receive'

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,18 @@
 // titulo e selector para mudar entre 'enviar' e 'receber'
 
-function Header() {
+interface HeaderProps {
+  setView: (view: "send" | "receive") => void;
+}
+
+function Header({ setView }: HeaderProps) {
   return (
     <header>
       <h1>Telecopy</h1>
 
       <div>
         <ol>
-          <li>Enviar</li>
-          <li>Receber</li>
+          <li onClick={() => setView("send")}>Enviar</li>
+          <li onClick={() => setView("receive")}>Receber</li>
         </ol>
       </div>
     </header>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,20 @@
 // Página principal
 
+import { useState } from "react";
 import CopyForm from "../components/CopyForm";
 import Header from "../components/Header";
 import ResultDisplay from "../components/ResultDisplay";
 
 function Home() {
+  const [view, setView] = useState<"send" | "receive">("send");
+
+  const onSetView = (view: "send" | "receive") => {
+    setView(view);
+  };
   return (
     <div className="home-container">
-      <Header />
-      <CopyForm />
-      <ResultDisplay />
+      <Header setView={onSetView} />
+      {view === "send" ? <CopyForm /> : <ResultDisplay />}
     </div>
   );
 }
